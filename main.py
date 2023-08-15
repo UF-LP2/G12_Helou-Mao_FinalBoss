e6a78fb94e47587907c7c3b5ab0e920a55a0a3c2
guadalupehelou <113600082+guadalupehelou@users.noreply.github.com>
2023-08-15 12:44:05 -0300

    lectura arhivo y division de barcos

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from src.lecturaArchivo import read_file
+from src.divisionBarcos import *
+from src.ship import Ship
+
+
 def main() -> None:
-  print("Hello World")
+    ship = [str]
+    ships = [Ship]
+    ship = read_file()
+    cruises = [Cruise]
+    cargo_ships = [Cargo]
+    cargo_ships = division_cargo_ships(ship)
+    ship = delete_ships(cargo_ships, ship)
+    cruises = division_cruises(ships)
+    ship = delete_ships(cruises, ship)
+
+    for x in ship:
+        ships.append(Ship(x))
+    print(ships)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- src/ship.py
+++ src/ship.py
@@ -1,11 +1,14 @@
 class Ship:  # draft: peso total , crew: personas
-    def __init__(self, draft, crew):
-        self.draft = draft
-        self.crew = crew
+    def __init__(self, draft=0.0, crew=0):
+        self.draft = float(draft)
+        self.crew = int(crew)
 
-    def is_worth_it (self) :
-        carga = self.draft - self.crew*1.5
-        if carga > 20:
+    def is_worth_it(self):
+        if self.draft < 0.0 or self.crew < 0:
+            raise ValueError("Error, no se aceptan valores negativos")
+
+        cargo = self.draft - self.crew*1.5
+        if cargo > 20:
             return True
         else:
             return False
