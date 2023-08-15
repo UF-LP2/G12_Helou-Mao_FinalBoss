17f4f7580c135d50394af87a4e3e0f73907a424f
Valentina Mao <maovalentina2004@gmail.com>
2023-08-14 23:03:01 -0300

    lectura archivo

--- src/lecturaArchivo.py
+++ src/lecturaArchivo.py
@@ -1,9 +1,25 @@
 
 import csv
-
+from cruise import Cruise
+from ship import Ship
+from cargo import Cargo
 def abrir(name):
     barcos = []
-    #with open(“ships.csv”) as file:
-    #    reader = csv.reader(file)
-    #    for row in reader:
-    #        (barcos.append(row))
+    extra = 0
+    crew = 0
+    quality = 0
+    draft = 0
+    cont = 0
+    with open(name) as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if cont != 0:
+                draft = row[0]
+                crew = row[1]
+                extra = row[2]
+                quality = row[3]
+                # vamos a tener que verlo mañana
+                if quality == 0 and extra == 0:
+                    barcos.append(Ship(draft, crew))
+            cont = cont + 1
+
